6501273cc1cdcc3dbd7205b9c0c7dd59d436ccc8
SilvanoNatalizi <silvanonataliziperugia@gmail.com>
2023-09-29 16:59:44 +0200

    complete load fake data script

--- scripts/load-fake-data.ts
+++ scripts/load-fake-data.ts
@@ -1,26 +1,76 @@
 import { Client } from "pg";
 import { loadEnvConfig } from "@next/env";
-
+import { faker } from "@faker-js/faker";
+import { cwd } from "node:process";
 
 const projectDir = process.cwd();
+
+console.log(`Current directory: ${cwd()}`);
+
 loadEnvConfig(projectDir);
 
-async function loadFakeData (numUsers: number =10) {
-    console.log(`executing load fake data generating ${numUsers} users`);
+async function loadFakeData(numUsers: number = 10) {
+  console.log(`executing load fake data generating ${numUsers} users`);
 
-    const client = new Client({
-        user: process.env.POSTGRES_USER,
-        host: process.env.POSTGRES_HOST,
-        database: process.env.POSTGRES_NAME,
-        password: process.env.POSTGRES_PASSWORD,
-        port: parseInt(process.env.POSTGRES_PORT!),
-    });
+  const client = new Client({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_NAME,
+    password: process.env.POSTGRES_PASSWORD,
+    port: parseInt(process.env.POSTGRES_PORT!),
+  });
 
-    await client.connect();
+  await client.connect();
 
-    const res = await client.query("select 1");
-    console.log(res);
+  try {
+    await client.query("begin");
+
+    for (let i = 0; i < numUsers; i++) {
+      console.log("sono qui");
+      console.log(`nome database= ${client.database}`);
+      await client.query(
+        `insert into users (username, password, avatar) values ($1, $2, $3)`,
+        [faker.internet.userName(), "password", faker.image.avatar()]
+      );
+    }
+
+    const res = await client.query(
+      "select id from public.users order by created_at desc limit $1",
+      [numUsers]
+    );
+    console.log(res.rows);
+
+    for (const row of res.rows) {
+      for (let i = 0; i < Math.ceil(Math.random() * 50); i++) {
+        await client.query(
+          "insert into posts (user_id, content) values ($1, $2)",
+          [row.id, faker.lorem.sentence()]
+        );
+      }
+    }
+
+    for (const row1 of res.rows) {
+      for (const row2 of res.rows) {
+        if ((row1.id != row2.id)) {
+          if (Math.random() > 0.5) {
+            await client.query(
+              "insert into follows (user_id, follower_id) values ($1, $2)",
+              [row1.id, row2.id]
+            );
+          }
+        }
+      }
+    }
+
+    await client.query("commit");
+  } catch (error) {
+    await client.query("rollback");
+    throw error;
+  } finally {
     await client.end();
+  }
 }
-loadFakeData();
+const numUsers = parseInt(process.argv[2]) || 10;
+console.log(`loading ${numUsers} fake users.`);
 
+loadFakeData(numUsers);
