e06973e685500e04861d8deae3024dd664710154
SilvanoNatalizi <silvanonataliziperugia@gmail.com>
2023-09-30 11:23:52 +0200

    load admin user. refactor db connection

--- scripts/load-fake-data.ts
+++ scripts/load-fake-data.ts
@@ -1,25 +1,14 @@
-import { Client } from "pg";
-import { loadEnvConfig } from "@next/env";
+
 import { faker } from "@faker-js/faker";
 import { cwd } from "node:process";
 import bcrypt from 'bcrypt'
+import { getClient } from "@/db";
 
-const projectDir = process.cwd();
-
-console.log(`Current directory: ${cwd()}`);
-
-loadEnvConfig(projectDir);
 
 async function loadFakeData(numUsers: number = 10) {
   console.log(`executing load fake data generating ${numUsers} users`);
 
-  const client = new Client({
-    user: process.env.POSTGRES_USER,
-    host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_NAME,
-    password: process.env.POSTGRES_PASSWORD,
-    port: parseInt(process.env.POSTGRES_PORT!),
-  });
+  const client =  await getClient();
 
   await client.connect();
 
