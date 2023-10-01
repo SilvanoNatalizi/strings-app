c3c1f6ba981775270e65641e82397fc5ea391dc3
SilvanoNatalizi <silvanonataliziperugia@gmail.com>
2023-10-01 23:19:37 +0200

    signin page

---
+++ app/(private)/feed/page.tsx
@@ -0,0 +1,9 @@
+import exp from "constants";
+
+export default async function Feed() {
+    return (
+        <main>
+            <h1>Feed</h1>
+        </main>
+    );
+}
---
+++ app/(public)/signin/page.tsx
@@ -0,0 +1,10 @@
+import Form from "./Form";
+
+export default async function SignIn() {
+    return (
+        <div>
+            <Form />
+            
+        </div>
+    )
+}
